a058209573094cdf3423dcd4b1a5e5eb88505701
Gergana <gergana.dobrichkova@gmail.com>
2023-04-08 18:53:00 +0300

    Created footer

---
+++ maistorbg/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <h3>© 2023 MaistorBG. Всички права запазени.</h3>
+      <p>Можете да ни откриете на следните контакти : </p>
+      <p>+ 359 885 638 441 </p>
+      <p>maistorBG@gmail.com</p>
+    </footer>
+  );
+}
+
+export default Footer;
