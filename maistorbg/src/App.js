7e05ab329e238078d32634afb98a752844d79cce
stTsStoyanov <stoyan112@gmail.com>
2023-04-12 15:28:02 +0300

    updated navigation created router and page templates

--- maistorbg/src/App.js
+++ maistorbg/src/App.js
@@ -4,23 +4,24 @@ import NavBar from "./components/Navigation/NavBar"
 import './App.scss';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
-// import HomePage from './pages/Home';
-// import DetailsPage from './pages/Details';
-// import NestedComponent from './components/NestedComponent/NestedComponent';
-// import ProfilePage from './pages/Profile';
+import HomePage from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import Offers from './pages/Offers/Offers';
+import CraftsMen from './pages/CraftsMen/CraftsMen';
+
 
 function App() {
   return ( <>
-    {/* <Navigation /> */}
+  
     <NavBar/>
     <Routes>
-      <Route index element={<Navigate to={'/home'} />}></Route>
-      {/* <Route path="/cocktails" element={<NestedComponent/>}>
-        <Route path={'catalog'} element={<HomePage />}></Route>
-        <Route path={'details/:id'} element={<DetailsPage />}></Route>
-      </Route>       */}
-      {/* <Route path="/profile" element={<ProfilePage/>}/> */}
-      {/* <Route path={'/filters'} element={<div>Filters screen</div>}></Route> */}
+      <Route index element={<Navigate to={'/home'}/>}></Route>
+      <Route path='/home' element={<HomePage/>}></Route>
+      <Route path='/register' element={<Register/>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/home/offers' element={<Offers/>}></Route>
+      <Route path='/home/craftsmen' element={<CraftsMen/>}></Route>
       <Route path={'*'} element={<div>NOT FOUND BRAT</div>}></Route>
     </Routes>
   </>
--- maistorbg/src/components/Navigation/NavBar.js
+++ maistorbg/src/components/Navigation/NavBar.js
@@ -6,22 +6,27 @@ function NavBar() {
     return (
         <Nav variant="pills" defaultActiveKey="/home" className='NavBar'>
             <Nav.Item>
-                <Nav.Link eventKey="link-1" as={Link} to="/home/register" className='NavLink'>
+                <Nav.Link eventKey="link-1" as={Link} to="/register" className='NavLink'>
                     Регистрация
                 </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="link-2" as={Link} to="/home/login" className='NavLink'>
+                <Nav.Link eventKey="link-2" as={Link} to="/login" className='NavLink'>
                     Вход
                 </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="link-3" as={Link} to="/home/offers" className='NavLink'>
+                <Nav.Link eventKey="link-3" as={Link} to="/home" className='NavLink'>
+                    Начало
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="link-4" as={Link} to="/home/offers" className='NavLink'>
                     Всички обяви
                     </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="link-4" as={Link} to="/home/craftsmen" className='NavLink'>
+                <Nav.Link eventKey="link-5" as={Link} to="/home/craftsmen" className='NavLink'>
                     Майстори
                     </Nav.Link>
             </Nav.Item>
