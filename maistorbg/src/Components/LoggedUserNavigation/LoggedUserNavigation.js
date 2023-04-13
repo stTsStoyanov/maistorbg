fc5e4d77d86f104f9bc741bde79fcb7b29206754
stTsStoyanov <stoyan112@gmail.com>
2023-04-13 20:15:21 +0300

    logout btn added to the navigation and onClick logout the user

--- maistorbg/src/Components/LoggedUserNavigation/LoggedUserNavigation.js
+++ maistorbg/src/Components/LoggedUserNavigation/LoggedUserNavigation.js
@@ -1,8 +1,14 @@
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 import "./LoggedUserNavigation.scss"
+import userManager from '../../model/managers/userManager';
 
 function NavBarLogged() {
+
+    const handlerLogout = () =>{
+        userManager.logout();
+    }
+
     return (
         <Nav variant="pills" defaultActiveKey="/home" className='NavBar'>
             <Nav.Item>
@@ -12,7 +18,7 @@ function NavBarLogged() {
             </Nav.Item>
 
             <Nav.Item>
-                <Nav.Link eventKey="link-3" as={Link} to="/home/offers" className='NavLink'>
+                <Nav.Link eventKey="link-2" as={Link} to="/home/offers" className='NavLink'>
                     Всички обяви
                     </Nav.Link>
             </Nav.Item>
@@ -20,6 +26,11 @@ function NavBarLogged() {
             <Nav.Item>
                 <Nav.Link eventKey="link-3" as={Link} to="/home/craftsmen" className='NavLink'>
                     Майстори
+                    </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="link-4" as={Link} to="/home" className='NavLink' onClick={handlerLogout}>
+                    Изход   
                     </Nav.Link>
             </Nav.Item>
 
