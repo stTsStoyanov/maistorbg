6124f44983aa78da86f4d4af139ed76c40f0478b
Gergana <gergana.dobrichkova@gmail.com>
2023-04-14 17:13:05 +0300

    Added functionality to add and change photos to my profile

--- maistorbg/src/Components/UserMyProfile/UserMyProfile.js
+++ maistorbg/src/Components/UserMyProfile/UserMyProfile.js
@@ -1,74 +1,8 @@
-// import React from "react";
-// import { Container, Row, Col, Button } from "react-bootstrap";
-// import { Link } from "react-router-dom"; // Import Link from React Router
-// import "./NavUser.scss"; // Import SCSS file
-// import userManager from "../../model/managers/userManager";
-
-// const ProfilePage = ({ userName, imageUrl }) => {
-
-//   const handlerLogout = () =>{
-//     userManager.logout();
-// }
-
-//   return (
-//     <Container>
-//       <Row>
-//         <Col>
-//           <div className="d-flex justify-content-center mt-4">
-//             <img
-//               src={
-//                 imageUrl ||
-//                 "https://via.placeholder.com/300x300.png?text=Default+Image"
-//               }
-//               alt="Profile"
-//               className="img-fluid profile-image"
-//             />
-//           </div>
-//           <div className="d-flex justify-content-center mt-4">
-//             <p className="display-4 text-center">
-//               Добре дошъл {userName} в твоя профил !
-//             </p>
-//           </div>
-//         </Col>
-//       </Row>
-//       <Row>
-//         <Col>
-//           <div className="d-flex justify-content-center">
-//             <Link to="/home/myprofile/user/history" className="btn btn-secondary btn-lg mx-3">
-//               <span className="btn-text">Твоята история</span>
-//               <span className="btn-icon"></span>
-//             </Link>
-//             <Link to="/home/myprofile/user/currentoffers" className="btn btn-secondary btn-lg mx-3">
-//               <span className="btn-text">Твоите обяви</span>
-//               <span className="btn-icon"></span>
-//             </Link>
-//             <Link to="/route3" className="btn btn-secondary btn-lg mx-3">
-//               <span className="btn-text">Твоята информация</span>
-//               <span className="btn-icon"></span>
-//             </Link>
-//           </div>
-//           <div className="d-flex justify-content-center mt-4" onClick={handlerLogout}>
-//             <Link to="/home" className="btn btn-secondary btn-lg">
-//               <span className="btn-text" style={{ whiteSpace: "pre-wrap" }}>
-//                 Изход профил
-//               </span>
-//               <span className="btn-icon"></span>
-//             </Link>
-//           </div>
-//         </Col>
-//       </Row>
-//     </Container>
-//   );
-// };
-
-// export default ProfilePage;
-
 import React, { useState } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-// import "./CraftManProfile.scss";
 import userManager from "../../model/managers/userManager";
-import "./UserMyProfile.scss"
+import "./UserMyProfile.scss";
 
 const handlerLogoutCraftsmen = () => {
   userManager.logout();
@@ -81,7 +15,23 @@ const UploadImage = ({ setImageUrl }) => {
 
     reader.onload = () => {
       setImageUrl(reader.result);
-      localStorage.setItem("profileImage", reader.result);
+      const users = JSON.parse(localStorage.getItem("users"));
+      const user = JSON.parse(localStorage.getItem("loggedUser"));
+      const updatedUsers = users.map((u) => {
+        if (u.id === user.id) {
+          return {
+            ...u,
+            photo: reader.result,
+          };
+        } else {
+          return u;
+        }
+      });
+      localStorage.setItem("users", JSON.stringify(updatedUsers));
+      localStorage.setItem("loggedUser", JSON.stringify({
+        ...user,
+        photo: reader.result,
+      }));
     };
 
     if (image) {
@@ -100,17 +50,15 @@ const UploadImage = ({ setImageUrl }) => {
   );
 };
 
-const CraftManProfile = ({ userName }) => {
-  const [imageUrl, setImageUrl] = useState(
-    localStorage.getItem("profileImage")
-  );
+const UserMyProfile = () => {
+  const user = JSON.parse(localStorage.getItem("loggedUser"));
+  const logged = user.username;
+  const photoPic = user.photo;
+  const [imageUrl, setImageUrl] = useState(photoPic);
 
   const handleClick = () => {
     document.getElementById("imageUpload").click();
   };
-  const user = JSON.parse(localStorage.getItem("loggedUser"));
-  const logged = user.username
-  ;
 
   return (
     <Container>
@@ -130,7 +78,7 @@ const CraftManProfile = ({ userName }) => {
           </div>
           <div className="d-flex justify-content-center mt-4">
             <p className="display-4 text-center">
-            Добре дошъл {logged} в твоя профил !
+              Добре дошъл {logged} в твоя профил !
             </p>
           </div>
         </Col>
@@ -178,5 +126,5 @@ const CraftManProfile = ({ userName }) => {
   );
 };
 
-export default CraftManProfile;
+export default UserMyProfile;
 
