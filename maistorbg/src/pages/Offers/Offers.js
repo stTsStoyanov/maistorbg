c3d0e21b1f91eb7d01a60b9165447e578bfa9bd9
Gergana <gergana.dobrichkova@gmail.com>
2023-04-17 23:15:57 +0300

    Added some style to All offers page and to Craftman choosing categories page

--- maistorbg/src/pages/Offers/Offers.js
+++ maistorbg/src/pages/Offers/Offers.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import OffersList from '../../components/AllOffers/AllOffers'
+import AllOffers from '../../components/AllOffers/AllOffers'
 
 export default function Offers() {
   return (
-    <OffersList/>
+    <AllOffers/>
   )
 }
