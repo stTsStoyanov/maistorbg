ed2babd9e5bb0bbec1ff5d5e3b23f81251a8ba5d
stTsStoyanov <stoyan112@gmail.com>
2023-04-18 10:09:14 +0300

    on home page when user is logged RegistrationPrompt is hide

--- maistorbg/src/pages/Home/Home.js
+++ maistorbg/src/pages/Home/Home.js
@@ -4,15 +4,18 @@ import TopCraftsmen from '../../components/HomePage/TopCraftsmen'
 import Review from '../../components/HomePage/Review'
 import Articles from '../../components/HomePage/Articles'
 import RegistrationPrompt from '../../components/HomePage/RegistrationPrompt'
+import localStorageManager from '../../model/managers/localStorageManager'
 
 export default function Home() {
+
+  const user = localStorageManager.loggedUser();
   return (
     // <div>Home</div>
     <>
       <TopCraftsmen/>
       <Review/>
       <Articles/>
-      <RegistrationPrompt/>
+      {user ? null : <RegistrationPrompt/>}
     </>
 
   )
