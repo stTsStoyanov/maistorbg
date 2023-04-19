7705657a393664a05ef5d661109bd19c5e2699a8
mkasabov98 <martinkasabov98@gmail.com>
2023-04-19 15:39:12 +0300

    updated the page where the customer is seeing his job advertisement and all the offers for the current advertisement, now the customer can accept the offer and the buttons is changed dinamically with text offer accepted/rejected

--- maistorbg/src/Components/CurrentJobAdvertisementsOffers/CurrentJobAdvertisementsOffers.js
+++ maistorbg/src/Components/CurrentJobAdvertisementsOffers/CurrentJobAdvertisementsOffers.js
@@ -58,11 +58,216 @@
 // }
 
 
+// import React, { useEffect, useState } from "react";
+// import { delayFunction } from "../../utilFunctions/utilFunctions";
+// import localStorageManager from "../../model/managers/localStorageManager";
+// import { Card } from "react-bootstrap";
+// import AcceptOfferButton from "./AcceptOfferButton/AcceptOfferButton";
+
+// export default function CurrentJobAdvertisementsOffers({ jobAdvertisement }) {
+//   const [offers, setOffers] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [acceptedOfferId, setAcceptedOfferId] = useState(null);
+
+//   useEffect(() => {
+//     delayFunction(localStorageManager.getItem, ["allOffers"]).then((allOffers) => {
+//       const jobOffers = allOffers.filter(
+//         (offer) => offer.jobAdvertisementId === jobAdvertisement.jobAdvertisementId
+//       );
+//       setOffers(jobOffers);
+//       setLoading(false);
+//     });
+//   }, [jobAdvertisement.jobAdvertisementId, offers]);
+
+//   const handleButtonClick = (offerId) => {
+//     setAcceptedOfferId(offerId);
+//     delayFunction(localStorageManager.getItem, ["allOffers"]).then((allOffers) => {
+//         const jobOffers = allOffers.filter(
+//           (offer) => offer.jobAdvertisementId === jobAdvertisement.jobAdvertisementId
+//         );
+//         setOffers(jobOffers);
+//         setLoading(false);
+//       });
+//   };
+
+//   const handleOfferRejection = (offerId) => {
+//     setAcceptedOfferId(offerId);
+//     // handle offer rejection logic here
+//   };
+
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <div>
+//       {offers.map((offer) => (
+//         <Card className="mb-4" key={offer.offerId}>
+//           <Card.Body className="d-flex flex-row">
+//             <div className="flex-grow-1">
+//               <Card.Title>{offer.offerText}</Card.Title>
+//               <Card.Subtitle className="mb-2 text-muted">{offer.offeredSum}</Card.Subtitle>
+//               <Card.Text>{offer.offeredTerm}</Card.Text>
+//               <Card.Text>{offer.creationDate}</Card.Text>
+//             </div>
+//             {offer.offerId === acceptedOfferId ? (
+//               offer.isAccepted ? (
+//                 <div>Offer accepted</div>
+//               ) : (
+//                 <div>Offer rejected</div>
+//               )
+//             ) : (
+//               <AcceptOfferButton offer={offer} onClick={() => handleButtonClick()}>
+//                 Accept
+//               </AcceptOfferButton>
+//             )}
+//           </Card.Body>
+//         </Card>
+//       ))}
+//     </div>
+//   );
+// }
+
+
+// import React, { useEffect, useState } from "react";
+// import { delayFunction } from "../../utilFunctions/utilFunctions";
+// import localStorageManager from "../../model/managers/localStorageManager";
+// import { Card } from "react-bootstrap";
+
+// export default function CurrentJobAdvertisementsOffers({ jobAdvertisement }) {
+//   const [offers, setOffers] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [acceptedOfferId, setAcceptedOfferId] = useState(null);
+
+//   useEffect(() => {
+//     delayFunction(localStorageManager.getItem, ["allOffers"]).then((allOffers) => {
+//       const jobOffers = allOffers.filter(
+//         (offer) => offer.jobAdvertisementId === jobAdvertisement.jobAdvertisementId
+//       );
+//       setOffers(jobOffers);
+//       setLoading(false);
+//     });
+//   }, [jobAdvertisement.jobAdvertisementId]);
+
+//   const acceptOffer = (offerId) => {
+//     setAcceptedOfferId(offerId);
+//     delayFunction(localStorageManager.getItem, ["allOffers"])
+//       .then((offers) => {
+//         const updatedOffers = offers.map((jobOffer) => {
+//           if (jobOffer.offerId === offerId) {
+//             return { ...jobOffer, isAccepted: true };
+//           } else if (jobOffer.jobAdvertisementId === jobAdvertisement.jobAdvertisementId) {
+//             return { ...jobOffer, isAccepted: false };
+//           }
+//           return jobOffer;
+//         });
+//         localStorageManager.setItem("allOffers", updatedOffers);
+//         setOffers(updatedOffers);
+//       });
+//   };
+
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <div>
+//       {offers.map((offer) => (
+//         <Card className="mb-4" key={offer.offerId}>
+//           <Card.Body className="d-flex flex-row">
+//             <div className="flex-grow-1">
+//               <Card.Title>{offer.offerText}</Card.Title>
+//               <Card.Subtitle className="mb-2 text-muted">{offer.offeredSum}</Card.Subtitle>
+//               <Card.Text>{offer.offeredTerm}</Card.Text>
+//               <Card.Text>{offer.creationDate}</Card.Text>
+//             </div>
+//             {offer.isAccepted ? (
+//               <div>Offer accepted</div>
+//             ) : (
+//               <button onClick={() => acceptOffer(offer.offerId)}>Accept</button>
+//             )}
+//           </Card.Body>
+//         </Card>
+//       ))}
+//     </div>
+//   );
+// }
+
+
+// import React, { useEffect, useState } from "react";
+// import { delayFunction } from "../../utilFunctions/utilFunctions";
+// import localStorageManager from "../../model/managers/localStorageManager";
+// import { Card } from "react-bootstrap";
+
+// export default function CurrentJobAdvertisementsOffers({ jobAdvertisement }) {
+//   const [offers, setOffers] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [acceptedOfferId, setAcceptedOfferId] = useState(null);
+
+//   useEffect(() => {
+//     delayFunction(localStorageManager.getItem, ["allOffers"]).then((allOffers) => {
+//       const jobOffers = allOffers.filter(
+//         (offer) => offer.jobAdvertisementId === jobAdvertisement.jobAdvertisementId
+//       );
+//       setOffers(jobOffers);
+//       setLoading(false);
+//     });
+//   }, [jobAdvertisement.jobAdvertisementId]);
+
+//   const acceptOffer = (offerId) => {
+//     setAcceptedOfferId(offerId);
+//     delayFunction(localStorageManager.getItem, ["allOffers"])
+//       .then((offers) => {
+//         const updatedOffers = offers.map((jobOffer) => {
+//           if (jobOffer.offerId === offerId) {
+//             return { ...jobOffer, isAccepted: true };
+//           } else if (
+//             jobOffer.jobAdvertisementId === jobAdvertisement.jobAdvertisementId &&
+//             jobOffer.isAccepted === null
+//           ) {
+//             return { ...jobOffer, isAccepted: false };
+//           }
+//           return jobOffer;
+//         });
+//         localStorageManager.setItem("allOffers", updatedOffers);
+//         setOffers(updatedOffers);
+//       });
+//   };
+
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <div>
+//       {offers.map((offer) => (
+//         <Card className="mb-4" key={offer.offerId}>
+//           <Card.Body className="d-flex flex-row">
+//             <div className="flex-grow-1">
+//               <Card.Title>{offer.offerText}</Card.Title>
+//               <Card.Subtitle className="mb-2 text-muted">{offer.offeredSum}</Card.Subtitle>
+//               <Card.Text>{offer.offeredTerm}</Card.Text>
+//               <Card.Text>{offer.creationDate}</Card.Text>
+//             </div>
+//             {offer.isAccepted === true ? (
+//               <div>Offer accepted</div>
+//             ) : offer.isAccepted === false ? (
+//               <div>Offer rejected</div>
+//             ) : (
+//               <button onClick={() => acceptOffer(offer.offerId)}>Accept</button>
+//             )}
+//           </Card.Body>
+//         </Card>
+//       ))}
+//     </div>
+//   );
+// }
+
+
 import React, { useEffect, useState } from "react";
 import { delayFunction } from "../../utilFunctions/utilFunctions";
 import localStorageManager from "../../model/managers/localStorageManager";
 import { Card } from "react-bootstrap";
-import AcceptOfferButton from "./AcceptOfferButton/AcceptOfferButton";
 
 export default function CurrentJobAdvertisementsOffers({ jobAdvertisement }) {
   const [offers, setOffers] = useState([]);
@@ -79,14 +284,21 @@ export default function CurrentJobAdvertisementsOffers({ jobAdvertisement }) {
     });
   }, [jobAdvertisement.jobAdvertisementId]);
 
-  const handleOfferAcceptance = (offerId) => {
+  const acceptOffer = (offerId) => {
     setAcceptedOfferId(offerId);
-    // handle offer acceptance logic here
-  };
-
-  const handleOfferRejection = (offerId) => {
-    setAcceptedOfferId(offerId);
-    // handle offer rejection logic here
+    delayFunction(localStorageManager.getItem, ["allOffers"])
+      .then((offers) => {
+        const updatedOffers = offers.map((jobOffer) => {
+          if (jobOffer.offerId === offerId) {
+            return { ...jobOffer, isAccepted: true };
+          } else if (jobOffer.jobAdvertisementId === jobAdvertisement.jobAdvertisementId) {
+            return { ...jobOffer, isAccepted: false };
+          }
+          return jobOffer;
+        });
+        localStorageManager.setItem("allOffers", updatedOffers);
+        setOffers(updatedOffers.filter((offer) => offer.jobAdvertisementId === jobAdvertisement.jobAdvertisementId));
+      });
   };
 
   if (loading) {
@@ -104,16 +316,11 @@ export default function CurrentJobAdvertisementsOffers({ jobAdvertisement }) {
               <Card.Text>{offer.offeredTerm}</Card.Text>
               <Card.Text>{offer.creationDate}</Card.Text>
             </div>
-            {offer.offerId === acceptedOfferId ? (
-              offer.isAccepted ? (
-                <div>Offer accepted</div>
-              ) : (
-                <div>Offer rejected</div>
-              )
-            ) : (
-              <AcceptOfferButton offer={offer} onAccept={handleOfferAcceptance} onReject={handleOfferRejection}>
-                Accept
-              </AcceptOfferButton>
+            {offer.isAccepted !== null && (
+              <div>{offer.isAccepted ? "Offer accepted" : "Offer rejected"}</div>
+            )}
+            {offer.isAccepted === null && (
+              <button onClick={() => acceptOffer(offer.offerId)}>Accept</button>
             )}
           </Card.Body>
         </Card>
