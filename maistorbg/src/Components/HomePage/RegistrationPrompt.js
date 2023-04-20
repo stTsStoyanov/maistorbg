1d9d51be67a652526cdeb714e68abef9ba4ef60c
stTsStoyanov <stoyan112@gmail.com>
2023-04-20 12:27:47 +0300

    minor changes on home page

--- maistorbg/src/Components/HomePage/RegistrationPrompt.js
+++ maistorbg/src/Components/HomePage/RegistrationPrompt.js
@@ -8,33 +8,35 @@ import { Container } from 'react-bootstrap';
 
 const RegistrationPrompt = () => {
     return (
-        <Container style={{ border: "1px solid black", padding: "20px" }} className="mx-md-5 my-5">
+        <div className='registrationPrompt'>
+            <Container style={{ border: "1px solid black", padding: "20px" }} className="mx-md-5 my-5">
 
-            <div className='containerReminder'>
-                <div className='register'>
-                    <Col xs={12} md={6}>
-                        <p>
-                            If you do not have an account, you can create one by clicking the button:
-                        </p>
-                    </Col>
-                    <Col xs={12} md={6}>
-                        <Button href="/register" block className='b'>Register</Button>
-                    </Col>
+                <div className='containerReminder'>
+                    <div className='register'>
+                        <Col xs={12} md={6}>
+                            <p>
+                                If you do not have an account, you can create one by clicking the button:
+                            </p>
+                        </Col>
+                        <Col xs={12} md={6}>
+                            <Button href="/register" block className='b'>Register</Button>
+                        </Col>
+                    </div>
+
+                    <div className='login'>
+                        <Col xs={12} md={6}>
+                            <p>
+                                If you have an account, login by clicking the button:
+                            </p>
+                        </Col>
+                        <Col xs={12} md={6}>
+                            <Button href="/login" block className='b'>Login</Button>
+                        </Col>
+                    </div>
                 </div>
 
-                <div className='login'>
-                    <Col xs={12} md={6}>
-                        <p>
-                            If you have an account, login by clicking the button:
-                        </p>
-                    </Col>
-                    <Col xs={12} md={6}>
-                        <Button href="/login" block className='b'>Login</Button>
-                    </Col>
-                </div>
-            </div>
-
-        </Container>
+            </Container>
+        </div>
     );
 };
 
