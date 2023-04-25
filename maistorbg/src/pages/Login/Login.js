44a66fe744b0cc48db4cb9b503231988710fcc49
stTsStoyanov <stoyan112@gmail.com>
2023-04-25 14:54:09 +0300

    login form alerts designed

--- maistorbg/src/pages/Login/Login.js
+++ maistorbg/src/pages/Login/Login.js
@@ -3,6 +3,7 @@ import { useNavigate, Link } from 'react-router-dom';
 import { Form, Button, Container, Row, Col, Alert } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import userManager from "../../model/managers/userManager"
+import "./Login.scss"
 
 const LoginForm = () => {
   const [formData, setFormData] = useState({
@@ -38,7 +39,7 @@ const LoginForm = () => {
       <Row className="justify-content-md-center">
         <Col md="6">
           <h1 className="text-center mb-4">Вход</h1>
-          <Form  onSubmit={handleSubmit}>
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="formBasicName">
               <Form.Label>Име</Form.Label>
               <Form.Control
@@ -62,6 +63,19 @@ const LoginForm = () => {
                 className="mb-3"
               />
             </Form.Group>
+
+            <div className='alerts-login'>
+
+              {loginError ?
+                // <div style={{ position: "absolute", bottom: "22", left: "48%", transform: "translateX(-40%)" }}>
+                  <Alert variant="danger" className='alertCustom'>
+                    <div className="inner-alert">{loginError}</div>
+                  </Alert>
+                : null
+              }
+
+            </div>
+
             <Link to={'/register'}><h4 className="text-center mb-4">Все още нямате регистрация?</h4></Link>
             <div className="text-center">
               <Button variant="primary" type="submit" className="btn btn-primary mb-3">
@@ -70,12 +84,7 @@ const LoginForm = () => {
             </div>
 
           </Form>
-          {loginError ?
-            <div style={{ position: "absolute", bottom: "22", left: "48%", transform: "translateX(-40%)" }}>
-              <Alert variant="danger">{loginError}</Alert>
-            </div>
-            : null
-          }
+
 
         </Col>
       </Row>
