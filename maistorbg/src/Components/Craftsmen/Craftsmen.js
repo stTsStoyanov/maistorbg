e189c09ec6369884cfcf4821121b0fb5e6065601
stTsStoyanov <stoyan112@gmail.com>
2023-04-25 09:55:04 +0300

    all craftsmen edit, no email or phone number when no logged user

--- maistorbg/src/Components/Craftsmen/Craftsmen.js
+++ maistorbg/src/Components/Craftsmen/Craftsmen.js
@@ -6,86 +6,87 @@ import "./Craftsmen.scss"
 import Advertisement from "./Advertisement";
 
 const Craftsman = () => {
-  const [craftsmenCategories, setCraftsmenCategories] = useState([]);
-  const [users, setUsers] = useState([]);
-  const [filteredUsers, setFilteredUsers] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [selectedCategory, setSelectedCategory] = useState(null);
-  const [selectedInput, setSelectedInput] = useState(null);
+    const [craftsmenCategories, setCraftsmenCategories] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [filteredUsers, setFilteredUsers] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [selectedInput, setSelectedInput] = useState(null);
 
-  const history = useNavigate();
-  const categoryRef = useRef(null);
-  const searchRef = useRef(null);
+    const history = useNavigate();
+    const categoryRef = useRef(null);
+    const searchRef = useRef(null);
+    const loggedUser = JSON.parse(localStorage.getItem('loggedUser')) || false;
 
-  useEffect(() => {
-    const categoriesFromLocalStorage = JSON.parse(localStorage.getItem("craftsmenCategories"));
-    setCraftsmenCategories(categoriesFromLocalStorage);
-    const usersFromLocalStorage = JSON.parse(localStorage.getItem("users"));
-    setUsers(usersFromLocalStorage);
-    setFilteredUsers(usersFromLocalStorage.filter(user => user.isClient === false));
-  }, []);
+    useEffect(() => {
+        const categoriesFromLocalStorage = JSON.parse(localStorage.getItem("craftsmenCategories"));
+        setCraftsmenCategories(categoriesFromLocalStorage);
+        const usersFromLocalStorage = JSON.parse(localStorage.getItem("users"));
+        setUsers(usersFromLocalStorage);
+        setFilteredUsers(usersFromLocalStorage.filter(user => user.isClient === false));
+    }, []);
 
-  useEffect(() => {
-    const filtered = users.filter(user => {
-      if (user.isClient) {
-        return false;
-      }
-      const skills = user.skills || [];
-      return skills.some(skill => skill.toLowerCase().includes(searchQuery.toLowerCase()));
-    });
-    setFilteredUsers(filtered);
-  }, [searchQuery, users]);
-
-
-
-  const handleSearch = debounce((event) => {
-    const searchValue = event.target.value.toLowerCase();
-    setSearchQuery(searchValue);
-  
-    const filteredData = filteredUsers.filter(item => {
-      const skills = item.skills || [];
-      return (
-        item?.name?.toLowerCase().includes(searchValue) ||
-        item?.email?.toLowerCase().includes(searchValue) ||
-        item?.phoneNumber?.includes(searchValue) ||
-        skills.some(skill => skill.toLowerCase().includes(searchValue))
-      );
-    });
-  
-    setSelectedInput(filteredData);
-  
-    if (searchRef.current) {
-      searchRef.current.scrollIntoView({ behavior: "smooth" });
-    }
-  }, 500);
-  
-  
-
-
-  const handleCategoryClick = (category) => {
-    setSelectedCategory(category);
-    const filtered = users.filter(user => {
-      if (user.isClient) {
-        return false;
-      }
-      const skills = user.skills || [];
-      return skills.includes(category.category);
-    });
-    setFilteredUsers(filtered);
-
-    if (categoryRef.current) {
-      categoryRef.current.scrollIntoView({ behavior: "smooth" });
-    }
-  };
+    useEffect(() => {
+        const filtered = users.filter(user => {
+            if (user.isClient) {
+                return false;
+            }
+            const skills = user.skills || [];
+            return skills.some(skill => skill.toLowerCase().includes(searchQuery.toLowerCase()));
+        });
+        setFilteredUsers(filtered);
+    }, [searchQuery, users]);
 
 
 
-  const handleClearFilters = () => {
-    setSelectedInput(null)
-    setSelectedCategory(null);
-    setSearchQuery("");
-    setFilteredUsers(users.filter(user => user.isClient === false));
-  };
+    const handleSearch = debounce((event) => {
+        const searchValue = event.target.value.toLowerCase();
+        setSearchQuery(searchValue);
+
+        const filteredData = filteredUsers.filter(item => {
+            const skills = item.skills || [];
+            return (
+                item?.name?.toLowerCase().includes(searchValue) ||
+                item?.email?.toLowerCase().includes(searchValue) ||
+                item?.phoneNumber?.includes(searchValue) ||
+                skills.some(skill => skill.toLowerCase().includes(searchValue))
+            );
+        });
+
+        setSelectedInput(filteredData);
+
+        if (searchRef.current) {
+            searchRef.current.scrollIntoView({ behavior: "smooth" });
+        }
+    }, 500);
+
+
+
+
+    const handleCategoryClick = (category) => {
+        setSelectedCategory(category);
+        const filtered = users.filter(user => {
+            if (user.isClient) {
+                return false;
+            }
+            const skills = user.skills || [];
+            return skills.includes(category.category);
+        });
+        setFilteredUsers(filtered);
+
+        if (categoryRef.current) {
+            categoryRef.current.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
+
+
+    const handleClearFilters = () => {
+        setSelectedInput(null)
+        setSelectedCategory(null);
+        setSearchQuery("");
+        setFilteredUsers(users.filter(user => user.isClient === false));
+    };
 
 
     return (
@@ -102,7 +103,7 @@ const Craftsman = () => {
                 {craftsmenCategories.map(category => (
                     <Col key={category.id} md={4} className="mb-3">
                         <Button variant="light" onClick={() => handleCategoryClick(category)}>
-                            <Image src={category.picture} thumbnail fluid style={{ width: "250px", height: "250px", borderRadius:"10%" }} />
+                            <Image src={category.picture} thumbnail fluid style={{ width: "250px", height: "250px", borderRadius: "10%" }} />
                             <h4 className="mt-3">{category.category}</h4>
                             <p className="">{category.information} </p>
                         </Button>
@@ -120,13 +121,17 @@ const Craftsman = () => {
                                 <Col key={index} md={4} className="mb-3">
                                     <Link to={`/home/craftsmen/${user.id}`}>
                                         <Card className="card-img">
-                                            <Card.Img variant="top" src={user.photo}  style={{ width: "250px", height: "300px" }} />
+                                            <Card.Img variant="top" src={user.photo} style={{ width: "250px", height: "285px" }} />
                                             <Card.Body>
                                                 <Card.Title>{user.name}</Card.Title>
+                                                {loggedUser && (
+                                                    <Card.Text>
+                                                        Телефонен номер: {user.phoneNumber}<br />
+                                                        Имейл адрес: {user.email}<br />
+                                                    </Card.Text>
+                                                )}
                                                 <Card.Text>
-                                                    Телефонен номер: {user.phoneNumber}<br />
-                                                    Имейл адрес: {user.email}<br />
-                                                    Рейтинг: {user.averageRating}<br />
+                                                    Рейтинг: {user.averageRating.toFixed(2)}<br />
                                                 </Card.Text>
                                             </Card.Body>
                                         </Card>
@@ -136,10 +141,11 @@ const Craftsman = () => {
                         </Row>
                     </Col>
                 )}
-                
+
             </Row >
 
-            
+
+
 
             <Row className="mt-3" ref={searchRef}>
                 {selectedInput && (
@@ -154,15 +160,19 @@ const Craftsman = () => {
                                     <Col key={index} md={4} className="mb-3">
                                         <Link to={`/home/craftsmen/${user.id}`}>
                                             <Card className="card-img">
-                                                <Card.Img variant="top" src={user.photo} style={{ width: "250px", height: "300px" }} />
+                                                <Card.Img variant="top" src={user.photo} style={{ width: "250px", height: "285px" }} />
                                                 <Card.Body>
                                                     <Card.Title>{user.name}</Card.Title>
+                                                    {loggedUser && (
+                                                        <Card.Text>
+                                                            Телефонен номер: {user.number}
+                                                            <br />
+                                                            Имейл адрес: {user.email}
+                                                            <br />
+                                                        </Card.Text>
+                                                    )}
                                                     <Card.Text>
-                                                    Телефонен номер: {user.number}
-                                                        <br />
-                                                        Имейл адрес: {user.email}
-                                                        <br />
-                                                        Рейтинг: {user.averageRating}
+                                                        Рейтинг: {user.averageRating.toFixed(2)}
                                                         <br />
                                                     </Card.Text>
                                                 </Card.Body>
@@ -181,7 +191,7 @@ const Craftsman = () => {
                 <Advertisement></Advertisement>
             </Row>
         </Container>
-        
+
     );
 };
 
